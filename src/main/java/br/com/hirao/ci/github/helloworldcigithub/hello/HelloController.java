16478a2062134a866ced3b47e418f925d78bbc00
LucasHirao <121697678+LucasHirao@users.noreply.github.com>
2023-01-31 20:13:06 -0300

    feat: Adicionado endpoint que consulta variavel de ambiente, apenas p… (#4)
    * feat: Adicionado endpoint que consulta variavel de ambiente, apenas para fins de aprendizado com configMap no Kubernetes
    
    * feat: Adicionado endpoint que consulta variavel de ambiente, apenas para fins de aprendizado com configMap no Kubernetes

--- src/main/java/br/com/hirao/ci/github/helloworldcigithub/hello/HelloController.java
+++ src/main/java/br/com/hirao/ci/github/helloworldcigithub/hello/HelloController.java
@@ -1,16 +1,27 @@
 package br.com.hirao.ci.github.helloworldcigithub.hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @RestController
 @RequestMapping("/")
 public class HelloController {
+    private final String env;
+
+    public HelloController(@Value("${helloworld.env}") String env) {
+        this.env = env;
+    }
+
     @GetMapping
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello from container");
+    }
+
+    @GetMapping("env")
+    public ResponseEntity<String> helloEnv() {
+        return ResponseEntity.ok(env);
     }
 }
--- src/test/java/br/com/hirao/ci/github/helloworldcigithub/hello/HelloControllerTest.java
+++ src/test/java/br/com/hirao/ci/github/helloworldcigithub/hello/HelloControllerTest.java
@@ -4,18 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class HelloControllerTest {
-
-    @Autowired
-    private HelloController helloController;
-
     @Autowired
     private TestRestTemplate testRestTemplate;
 
@@ -26,5 +23,11 @@ public class HelloControllerTest {
     void testHello() {
         var resultado = testRestTemplate.getForObject("http://localhost:" + port+ "/", String.class);
         assertEquals("Hello from container", resultado);
+    }
+
+    @Test
+    void testEnv() {
+        var resultado = testRestTemplate.getForObject("http://localhost:" + port+ "/env", String.class);
+        assertEquals("test", resultado);
     }
 }
